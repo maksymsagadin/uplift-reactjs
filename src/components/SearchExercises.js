e66e36d7166f9686ff13c6fc19ed6c80a84b0475
Maksym Sagadin <maxsagadin@gmail.com>
2022-07-26 17:48:31 -0700

    added search bar

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -4,11 +4,25 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material'
 
 const SearchExercises = () => {
   return (
-    <Stack alignItems='center' justifyContent='center' p='20px' mt='150px'>
-        <Typography>
+    <Stack alignItems='center' justifyContent='center' p='20px' mt='250px'>
+        <Typography fontWeight={700} sx={{ fontSize:{ lg: '44px', xs: '30px' } }} mb="49px" textAlign="center">
             Give These <br />
             Exercises A Try
         </Typography>
+        <Box position='relative'>
+            <TextField
+                sx={{ input: { fontWeight: '700', border: 'none', borderRadius: '4px' }, width: { lg: '1170px', xs: '350px' }, backgroundColor: '#fffbfd', borderRadius: '4px' }}
+                height='76px'
+                value=''
+                onChange={(e) => {}}
+                placeholder="Search Exercises"
+                type="text"
+            />
+            <Button className="button" sx={{ bgcolor: '#a2a2c7', color: '#fffbfd', textTransform: 'none', width: { lg: '173px', xs: '80px' }, height: '56px', position: 'absolute', right: '0', fontSize: { lg: '20px', xs: '14px' } }} onClick={(e) => {}}>
+                Search
+            </Button>
+                
+        </Box>
     </Stack>
   )
 }
