541c374b7bf4d76e229b79d1387adcf10c938beb
Maksym Sagadin <maxsagadin@gmail.com>
2022-08-29 17:31:01 -0700

    init

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -12,7 +12,7 @@ export const youtubeOptions = {
 		'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
 		'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
 	}
-};
+}
 
 export const fetchData = async ( url, options ) => {
     const response = await fetch( url, options )
