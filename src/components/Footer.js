71a97fd49125fcf1129b2b80aca27ea692e82535
Maksym Sagadin <maxsagadin@gmail.com>
2022-10-04 11:41:08 -0700

    scroll to top of page on click of logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,17 @@ import { Box, Stack, Typography } from '@mui/material'
 import Logo from '../assets/logos/wolf.png'
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0, 
+      behavior: 'smooth'
+    })
+  }
+
   return (
     <Box>
       <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
-        <img src={Logo} alt="logo" style={{ width: '150px', height: '150px' }} />
+        <img onClick={scrollToTop} src={Logo} alt="logo" style={{ width: '150px', height: '150px', cursor: 'pointer' }} />
       </Stack>
       <Typography variant="h5" sx={{ fontSize: { lg: '28px', xs: '20px' } }} mt="41px" textAlign="center" pb="40px">Your greatest Wealth is your Health.</Typography>
     </Box>
